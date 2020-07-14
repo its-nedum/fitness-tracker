96ffd3795ef11f86d10023248842c3ad7ed37be2
its-nedum <de.magnificent1@gmail.com>
2020-07-14 09:18:49 +0100

    features(styled the login and signup page)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # home page
-  root "activitys#index"
+  # home page routes
+  root "pages#home"
 
   # devise routes
   devise_for :users
