21bb962c44270e44d1f73f7f901f5c04326e84dc
its-nedum <de.magnificent1@gmail.com>
2020-07-15 16:41:32 +0100

    chores(started user profile update)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_091449) do
+ActiveRecord::Schema.define(version: 2020_07_15_150403) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 2020_07_14_091449) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "firstname"
+    t.string "lastname"
+    t.string "gender"
+    t.text "location"
+    t.text "dob"
+    t.text "bio"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
