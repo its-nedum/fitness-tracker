7fadad5445eadaefa3700b664ff7c1537414cf85
its-nedum <de.magnificent1@gmail.com>
2020-07-21 15:13:13 +0100

    features(implemented image upload)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
    def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password]) 
-        devise_parameter_sanitizer.permit(:account_update, keys: [:email,:password,:current_password,:firstname,:lastname,:gender,:dob,:location,:bio])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:email,:password,:current_password,:firstname,:lastname,:gender,:dob,:location,:bio,:image])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :firstname, :lastname, presence: true
+
+  has_one_attached :image
 end
