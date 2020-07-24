c947a6b3ef2c287c43b1210244748dadee8670b0
its-nedum <de.magnificent1@gmail.com>
2020-07-24 17:06:55 +0100

    chores(fixed some issue)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-  
+
   # EMAIL CONFIG 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
@@ -40,7 +40,8 @@ Rails.application.configure do
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password:ENV["GMAIL_PASSWORD"]
-  }config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
