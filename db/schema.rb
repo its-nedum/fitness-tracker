a7a6ad8ad2b257d50dfc8db9185601508b3ac282
its-nedum <de.magnificent1@gmail.com>
2020-08-03 13:58:52 +0100

    chores(time picker implemented)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_105010) do
+ActiveRecord::Schema.define(version: 2020_08_03_114725) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 2020_07_23_105010) do
     t.string "distance"
     t.string "duration"
     t.string "elevation"
-    t.text "date"
-    t.text "time"
+    t.date "date"
+    t.time "time"
     t.string "exertion"
     t.string "user"
     t.datetime "created_at", precision: 6, null: false
